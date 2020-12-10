8ddb881256bbe9b1ca0fdd8c4fa0b9d19a4523ef
Maxim Zavidei <github@maximzavidei.anonaddy.me>
2020-12-10 11:00:32 +0100

    Improved MyDate class.

--- single-user-system/src/model/MyDate.java
+++ single-user-system/src/model/MyDate.java
@@ -8,33 +8,31 @@ public class MyDate
   private int month;
   private int year;
 
-  public MyDate(int day, int month, int year)
-  {
+  public MyDate(int day, int month, int year) {
     set(day, month, year);
   }
 
-  public MyDate()
-  {
+  public MyDate() {
     LocalDate today = LocalDate.now();
 
     this.day = today.getDayOfMonth();
     this.month = today.getMonthValue();
     this.year = today.getYear();
   }
-  public void set(int day, int month, int year)
-  {
-    if (year < 0)
-    {
+
+  public void set(int day, int month, int year) {
+    if (year < 0) {
       year = -year;
     }
-    if (month < 1)
-    {
+
+    if (month < 1) {
       month = 1;
     }
-    else if (month > 12)
-    {
+
+    else if (month > 12) {
       month = 12;
     }
+
     this.year = year;
     this.month = month;
     if (day < 1)
@@ -47,18 +45,16 @@ public class MyDate
     }
     this.day = day;
   }
-  public int getDay()
-  {
+
+  public int getDay() {
     return day;
   }
 
-  public int getMonth()
-  {
+  public int getMonth() {
     return month;
   }
 
-  public int getYear()
-  {
+  public int getYear() {
     return year;
   }
 
@@ -67,65 +63,34 @@ public class MyDate
     return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
   }
 
-  public String getMonthName()
-  {
-    switch (month)
-    {
-      case 1:
-        return "January";
-      case 2:
-        return "February";
-      case 3:
-        return "March";
-      case 4:
-        return "April";
-      case 5:
-        return "May";
-      case 6:
-        return "June";
-      case 7:
-        return "July";
-      case 8:
-        return "August";
-      case 9:
-        return "September";
-      case 10:
-        return "October";
-      case 11:
-        return "November";
-      case 12:
-        return "December";
-      default:
-        return "Error";
-    }
+  public String getMonthName() {
+    return new String[] {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    }[month - 1];
   }
-  public int numberOfDaysInMonth()
-  {
-    switch (month)
-    {
-      case 2:
-        if (isLeapYear())
-        {
-          return 29;
-        }
-        return 28;
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        return 30;
-      default:
-        return 31;
-    }
+
+  public int numberOfDaysInMonth() {
+    return 31 - (this.month == 2 ? isLeapYear() ? 2 : 3 : (this.month - 1) % 7 % 2);
   }
-  public boolean isBefore(MyDate other)
-  {
+
+  public boolean isBefore(MyDate other) {
     int dummyDaysOfThis = year * 400 + month * 31 + day;
     int dummyDaysOfOther = other.year * 400 + other.month * 31 + other.day;
     return dummyDaysOfThis < dummyDaysOfOther;
   }
-  public void stepForward()
-  {
+
+  public void stepForward() {
     day++;
     if (day > numberOfDaysInMonth())
     {
@@ -139,15 +104,11 @@ public class MyDate
     }
   }
 
-  public void stepForward(int days)
-  {
-    for (int i = 0; i < days; i++)
-    {
-      stepForward();
-    }
+  public void stepForward(int days) {
+    while (days-- != 0) stepForward();
   }
-  public String dayOfWeek()
-  {
+
+  public String dayOfWeek() {
     int q = day;
     int m = 0;
     switch (getMonthName())
@@ -262,24 +223,17 @@ public class MyDate
 
   }
 
-  public MyDate copy()
-  {
-    MyDate other = new MyDate(day, month, year);
-    return other;
+  public MyDate copy() {
+    return new MyDate(day, month, year);
   }
 
-  public boolean equals(Object obj)
-  {
-    if (!(obj instanceof MyDate))
-    {
-      return false;
-    }
+  public boolean equals(Object obj) {
+    if (!(obj instanceof MyDate)) return false;
     MyDate other = (MyDate) obj;
     return day == other.day && month == other.month && year == other.year;
   }
-  public String toString()
-  {
+
+  public String toString() {
     return String.format("%02d/%02d/%02d", day, month, year);
   }
-
 }

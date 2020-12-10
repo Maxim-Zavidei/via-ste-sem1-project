8cc2586f12e842ee408eca32c1fe29c2dcbe2321
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2020-12-10 19:17:39 +0100

    Formatted, commented and finalized MyDate class.

--- single-user-system/src/model/MyDate.java
+++ single-user-system/src/model/MyDate.java
@@ -2,66 +2,81 @@ package model;
 
 import java.time.LocalDate;
 
+/**
+ * A class to store and process date values.
+ */
 public class MyDate {
 
   private int day;
   private int month;
   private int year;
 
+  /**
+   * Constructor with defined date values.
+   * @param day Value between [1; 31] representing the day.
+   * @param month Value between [1; 12] representing the month.
+   * @param year Value between [1; +inf] representing the year.
+   * @throws IllegalArgumentException if any of the arguments are invalid.
+   */
   public MyDate(int day, int month, int year) {
     set(day, month, year);
   }
 
+  /**
+   * Constructor with current os date values.
+   */
   public MyDate() {
     LocalDate today = LocalDate.now();
-
-    this.day = today.getDayOfMonth();
-    this.month = today.getMonthValue();
-    this.year = today.getYear();
+    day = today.getDayOfMonth();
+    month = today.getMonthValue();
+    year = today.getYear();
   }
 
+  /**
+   * Setter for all instance variables of the class.
+   * @param day Value between [1; 31] representing the day.
+   * @param month Value between [1; 12] representing the month.
+   * @param year Value between [1; +inf] representing the year.
+   * @throws IllegalArgumentException if any of the arguments are invalid.
+   */
   public void set(int day, int month, int year) {
-    if (year < 0) {
-      year = -year;
-    }
-
-    if (month < 1) {
-      month = 1;
-    }
-
-    else if (month > 12) {
-      month = 12;
-    }
+    if (year < 0) throw new IllegalArgumentException("Invalid year argument.");
+    if (month < 1 || 12 < month) throw new IllegalArgumentException("Invalid month argument.");
+    if (day < 1 || 31 < day) throw new IllegalArgumentException("Invalid day argument.");
 
     this.year = year;
     this.month = month;
-    if (day < 1)
-    {
-      day = 1;
-    }
-    else if (day > numberOfDaysInMonth())
-    {
-      day = numberOfDaysInMonth();
-    }
     this.day = day;
   }
 
+  /**
+   * Getter for day instance variable.
+   * @return Value between [1; 31] representing the day.
+   */
   public int getDay() {
     return day;
   }
 
+  /**
+   * Getter for month instance variable.
+   * @return Value between [1; 12] representing the month.
+   */
   public int getMonth() {
     return month;
   }
 
+  /**
+   * Getter for year instance variable.
+   * @return Value between [1; +inf] representing the year.
+   */
   public int getYear() {
     return year;
   }
 
-  public boolean isLeapYear() {
-    return year % 4 == 0 && ((year % 100 != 0) || (year % 400 == 0));
-  }
-
+  /**
+   * Returns the english name for the month of the date.
+   * @return A string with the month name.
+   */
   public String getMonthName() {
     return new String[] {
         "January",
@@ -79,160 +94,90 @@ public class MyDate {
     }[month - 1];
   }
 
+  /**
+   * Checks if the year is a leap year.
+   * @return Whether the leap is year or not.
+   */
+  public boolean isLeapYear() {
+    return (year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0);
+  }
+
+  /**
+   * Calculates the number of days in the month.
+   * @return Value between [28, 31] representing the number of days in month.
+   */
   public int numberOfDaysInMonth() {
-    return 31 - (this.month == 2 ? isLeapYear() ? 2 : 3 : (this.month - 1) % 7 % 2);
+    return 31 - (month == 2 ? isLeapYear() ? 2 : 3 : (month - 1) % 7 % 2);
   }
 
+  /**
+   * Checks whether the date is before the argument date.
+   * @param other A date to compare to.
+   * @return Whether the date is before argument date.
+   * @throws IllegalArgumentException if the date argument is null.
+   */
   public boolean isBefore(MyDate other) {
-    int dummyDaysOfThis = year * 400 + month * 31 + day;
-    int dummyDaysOfOther = other.year * 400 + other.month * 31 + other.day;
-    return dummyDaysOfThis < dummyDaysOfOther;
+    if (other == null) throw new IllegalArgumentException("Null date argument.");
+    return 366 * (other.year - year) + 31 * (other.month - month) + other.day - day > 0;
   }
 
+  /**
+   * Sets the date to the date of the next day.
+   */
   public void stepForward() {
-    day++;
-    if (day > numberOfDaysInMonth())
-    {
-      day = 1;
-      month++;
-      if (month > 12)
-      {
-        month = 1;
-        year++;
-      }
-    }
+    day = day + 1 > numberOfDaysInMonth() ? month + 1 > 12 ? month = -year + ++year : -month + ++month : ++day;
   }
 
+  /**
+   * Sets the date to the date after next days argument.
+   * @param days Value between [1, +inf] representing number of days to skip.
+   * @throws IllegalArgumentException if the days argument is invalid.
+   */
   public void stepForward(int days) {
+    if (days < 1) throw new IllegalArgumentException("Invalid argument for days to skip.");
     while (days-- != 0) stepForward();
   }
 
+  /**
+   * Returns the english name for the day of the date.
+   * @return A string with the day name.
+   */
   public String dayOfWeek() {
-    int q = day;
-    int m = 0;
-    switch (getMonthName())
-    {
-      case "January":
-        m = 13;
-        break;
-      case "February":
-        m = 14;
-        break;
-      case "March":
-        m = 3;
-        break;
-      case "April":
-        m = 4;
-        break;
-      case "May":
-        m = 5;
-        break;
-      case "June":
-        m = 6;
-        break;
-      case "July":
-        m = 7;
-        break;
-      case "August":
-        m = 8;
-        break;
-      case "September":
-        m = 9;
-        break;
-      case "October":
-        m = 10;
-        break;
-      case "November":
-        m = 11;
-        break;
-      case "December":
-        m = 12;
-        break;
-
-    }
-    int k = 0;
-    switch (getMonthName())
-    {
-      case "March":
-      case "April":
-      case "May":
-      case "June":
-      case "July":
-      case "August":
-      case "September":
-      case "October":
-      case "November":
-      case "December":
-        k = year % 100;
-        break;
-      case "January":
-      case "February":
-        k = (year - 1) % 100;
-        break;
-
-    }
-    int j = 0;
-    switch (getMonthName())
-    {
-      case "March":
-      case "April":
-      case "May":
-      case "June":
-      case "July":
-      case "August":
-      case "September":
-      case "October":
-      case "November":
-      case "December":
-        j = year / 100;
-        break;
-      case "January":
-      case "February":
-        j = (year - 1) / 100;
-        break;
-
-    }
-    int h = (q + ((13 * (m + 1)) / 5) + k + (k / 4) + (j / 4) + (5 * j)) % 7;
-    String t = null;
-    switch (h)
-    {
-      case 0:
-        t = "Saturday";
-        break;
-      case 1:
-        t = "Sunday";
-        break;
-      case 2:
-        t = "Monday";
-        break;
-      case 3:
-        t = "Tuesday";
-        break;
-      case 4:
-        t = "Wednesday";
-        break;
-      case 5:
-        t = "Thursday";
-        break;
-      case 6:
-        t = "Friday";
-        break;
-    }
-    return t;
-
+    return new String[] {
+        "Saturday",
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+    }[(day + (int) Math.round(2.6 * month - 0.2) + year + (year / 4) + (month / 400) - ((month / 100) * 2)) % 7];
   }
 
+  /**
+   * Copies the date object.
+   * @return The copied date object.
+   */
   public MyDate copy() {
     return new MyDate(day, month, year);
   }
 
-  public boolean equals(Object obj) {
+  /**
+   * Checks whether the date is equaled to the argument object.
+   * @param obj Should be an object of type MyDate.
+   * @return Whether the date is equaled to the argument object
+   */
+  @Override public boolean equals(Object obj) {
     if (!(obj instanceof MyDate)) return false;
     MyDate other = (MyDate) obj;
     return day == other.day && month == other.month && year == other.year;
   }
 
-  public String toString() {
+  /**
+   * Returns a string with the the formatted date.
+   * @return A string representing the date formatted as dd/mm/yyyy.
+   */
+  @Override public String toString() {
     return String.format("%02d/%02d/%02d", day, month, year);
   }
 }

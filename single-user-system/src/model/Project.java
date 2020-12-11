2704062721a61ed5e2b10808c18a4c22110c8cdd
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2020-12-11 14:15:43 +0100

    Fixed and commented the constructors for the project class.

--- single-user-system/src/model/Project.java
+++ single-user-system/src/model/Project.java
@@ -2,15 +2,13 @@ package model;
 
 import java.util.ArrayList;
 
-public class Project
-{
+public class Project {
 
   private String id;
   private String title;
   private String description;
-  private String status;
-  private MyDate date;
-  private String role;
+  private float status;
+  private MyDate deadline;
   private MemberList memberList;
   private RequirementList requirementList;
   private TaskList taskList;
@@ -18,28 +16,51 @@ public class Project
   private Member productOwner;
   private Member scrumMaster;
 
-  public Project(String id, String description, String title, int day,
-      int month, int year, String status)
-  {
+  /**
+   * Constructor with extended number of defined values.
+   * @param id Id value of the project.
+   * @param title Title for the project.
+   * @param description Description of the project.
+   * @param day The day of the project's deadline.
+   * @param month The month of the project's deadline.
+   * @param year The year of the project's deadline.
+   * @throws IllegalArgumentException If date arguments are invalid.
+   * @throws IllegalArgumentException If deadline is in the past.
+   */
+  public Project(String id, String title, String description, int day, int month, int year) {
+    MyDate deadline = new MyDate(day, month, year);
+    if (deadline.isBefore(new MyDate())) throw new IllegalArgumentException("The deadline must be set to a future date.");
+
     this.id = id;
-    this.description = description;
-    date = new MyDate(day, month, year);
-    this.title = title;
-    this.status = status;
-  }
-
-  public Project(String title, String description, int day, int month, int year,
-      String role)
-  {
     this.title = title;
     this.description = description;
-    date = new MyDate(day, month, year);
-    this.role = role;
+    status = 0;
+    this.deadline = deadline;
+    memberList = new MemberList();
+    requirementList = new RequirementList();
+    taskList = new TaskList();
+    projectCreator = null;
+    productOwner = null;
+    scrumMaster = null;
   }
 
-  public boolean addTask(String id, String title, String description, int day,
-      int month, int year)
-  {
+  /**
+   * Constructor with minimal number of defined values.
+   * @param id Id value of the project.
+   * @param title Title for the project.
+   * @param day The day of the project's deadline.
+   * @param month The month of the project's deadline.
+   * @param year The year of the project's deadline.
+   * @throws IllegalArgumentException If date arguments are invalid.
+   * @throws IllegalArgumentException If deadline is in the past.
+   */
+  public Project(String id, String title, int day, int month, int year) {
+    this(id, title, "", day, month, year);
+  }
+
+
+  public boolean addTask(String id, String title, String description, int day, int month, int year) {
+
     Task task = new Task(id, title, description, new MyDate(day, month, year));
 
     if (taskList.addTask(task))
@@ -50,26 +71,7 @@ public class Project
     return false;
   }
 
-  public boolean removeTask(Task task)
-  {
-    if (taskList.removeTask(task))
-    {
-      return true;
-    }
-    return false;
-  }
-
-  public boolean removeTask(String id)
-  {
-    if (taskList.removeTask(id))
-    {
-      return true;
-    }
-    return false;
-  }
-
-  public boolean addRequirement(Requirement requirement)
-  {
+  public boolean addRequirement(Requirement requirement) {
     for (Requirement temp : requirementList.getAllApprovedRequirements())
     {
       if (temp.equals(requirement))
@@ -80,6 +82,22 @@ public class Project
 
     requirementList.addRequirement(requirement);
     return true;
+  }
+
+  public boolean removeTask(Task task) {
+    if (taskList.removeTask(task))
+    {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean removeTask(String id) {
+    if (taskList.removeTask(id))
+    {
+      return true;
+    }
+    return false;
   }
 
   public String getTitle()

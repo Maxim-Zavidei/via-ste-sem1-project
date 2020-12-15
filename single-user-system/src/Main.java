97062405ac2e239d293a6ad4e18db234da40fb15
jaime <jaime.elenatristan@gmail.com>
2020-12-15 13:37:08 +0100

    GG wp team 1

--- single-user-system/src/Main.java
+++ single-user-system/src/Main.java
@@ -1,6 +1,31 @@
-public class Main
-{
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception{
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("MMMMMM");
+    primaryStage.setScene(new Scene(root, 1200, 675));
+    primaryStage.show();
+    primaryStage.setMaxWidth(primaryStage.getWidth());
+    primaryStage.setMinWidth(primaryStage.getWidth());
+    primaryStage.setMaxHeight(primaryStage.getHeight());
+    primaryStage.setMinHeight(primaryStage.getHeight());
+  }
+
+
+
+
+
+
   public static void main(String[] args) {
-    System.out.println("Single System!");
+    launch(args);
   }
 }

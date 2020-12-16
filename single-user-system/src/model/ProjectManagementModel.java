14501dbe83be29907baec69c54c31111fd43560b
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2020-12-16 14:15:34 +0100

    Cleanup in model interface and manager.

--- single-user-system/src/model/ProjectManagementModel.java
+++ single-user-system/src/model/ProjectManagementModel.java
@@ -8,7 +8,7 @@ public interface ProjectManagementModel {
   ArrayList<Project> getAllProjects();
   ArrayList<Project> getProjectsWithStatusOver(float status);
   ArrayList<Project> getProjectsBeforeDeadline(MyDate deadline);
-  Project getProjectById(String Id);
+  Project getProjectById(String id);
 
   // ------------------------------ Getters for Requirements ------------------------------
   ArrayList<Requirement> getAllRequirements();
@@ -18,7 +18,7 @@ public interface ProjectManagementModel {
   ArrayList<Requirement> getRequirementsLinkedTo(Member member);
   ArrayList<Requirement> getRequirementsWithStatusOver(float status);
   ArrayList<Requirement> getRequirementsBeforeDeadline(MyDate deadline);
-  Requirement getRequirementById(String Id);
+  Requirement getRequirementById(String id);
 
   // ------------------------------ Getters for Tasks ------------------------------
   ArrayList<Task> getAllTasks();
@@ -27,7 +27,7 @@ public interface ProjectManagementModel {
   ArrayList<Task> getTasksLinkedTo(Member member);
   ArrayList<Task> getTasksByStatus(String status);
   ArrayList<Task> getTasksBeforeDeadline(MyDate deadline);
-  Task getTaskById(String Id);
+  Task getTaskById(String id);
 
   // ------------------------------ Getters for Members ------------------------------
   ArrayList<Member> getAllMembers();
@@ -41,7 +41,7 @@ public interface ProjectManagementModel {
   Project addProject(String title, String description, int day, int month, int year);
   Project addProject(String title, int day, int month, int year);
   void removeProject(Project project);
-  void removeProject(String Id);
+  void removeProject(String id);
   Member addMember(String firstName, String lastName, MyDate birthday,String email);
   Member addMember(String firstName, String lastName, MyDate birthday,String email, long telephoneNumber);
   void removeMember(String firstName, String lastName);
--- single-user-system/src/model/ProjectManagementModelManager.java
+++ single-user-system/src/model/ProjectManagementModelManager.java
@@ -2,213 +2,160 @@ package model;
 
 import java.util.ArrayList;
 
-public class ProjectManagementModelManager implements ProjectManagementModel
-{
+public class ProjectManagementModelManager implements ProjectManagementModel {
+
   private ProjectList projectList;
   private MemberList memberList;
 
-  public ProjectManagementModelManager()
-  {
+  // ------------------------------ Constructors ------------------------------
+
+  public ProjectManagementModelManager() {
     projectList = new ProjectList();
     memberList = new MemberList();
   }
-  public ArrayList<Project> getAllProjects()
-  {
-     return projectList.getAllProjects();
+
+  // ------------------------------ Getters for Projects ------------------------------
+
+  public ArrayList<Project> getAllProjects() {
+    return projectList.getAllProjects();
   }
 
-  public ArrayList<Project> getProjectsWithStatusOver(float status)
-  {
-   return   projectList.getAllProjectsWithStatusOver(status);
+  public ArrayList<Project> getProjectsWithStatusOver(float status) {
+    return projectList.getAllProjectsWithStatusOver(status);
   }
 
-  public ArrayList<Project> getProjectsBeforeDeadline(MyDate deadline)
-  {
+  public ArrayList<Project> getProjectsBeforeDeadline(MyDate deadline) {
     return projectList.getAllProjectsBeforeDeadline(deadline);
   }
 
-  public Project getProjectById(String Id)
-  {
-    return projectList.getProjectById(Id);
+  public Project getProjectById(String id) {
+    return projectList.getProjectById(id);
   }
 
-  public ArrayList<Requirement> getAllRequirements()
-  {
+  // ------------------------------ Getters for Requirements ------------------------------
+
+  public ArrayList<Requirement> getAllRequirements() {
     return projectList.getAllRequirements();
   }
 
-  public ArrayList<Requirement> getAllRequirementsWithPriority(String priorityGroup)
-  {
+  public ArrayList<Requirement> getAllRequirementsWithPriority(String priorityGroup) {
     return projectList.getAllRequirementsWithPriority(priorityGroup);
   }
 
-  public ArrayList<Requirement> getRequirementsLinkedTo(Project project)
-  {
+  public ArrayList<Requirement> getRequirementsLinkedTo(Project project) {
     return project.getAllRequirements();
   }
 
-  public ArrayList<Requirement> getRequirementsLinkedTo(Task task)
-  {
+  public ArrayList<Requirement> getRequirementsLinkedTo(Task task) {
     return task.getAllAssignedRequirements();
   }
-  public ArrayList<Requirement> getRequirementsLinkedTo(Member member)
-  {
-    ArrayList<Requirement> requirements = new ArrayList<>();
 
-    for (Task task : member.getAllAssignedTasks()) requirements.addAll(task.getAllAssignedRequirements());
-
-    return requirements;
+  public ArrayList<Requirement> getRequirementsLinkedTo(Member member) {
+    ArrayList<Requirement> requirementsToReturn = new ArrayList<>();
+    for (Task task : member.getAllAssignedTasks()) requirementsToReturn.addAll(task.getAllAssignedRequirements());
+    return requirementsToReturn;
   }
 
-  public ArrayList<Requirement> getRequirementsWithStatusOver(float status)
-  {
+  public ArrayList<Requirement> getRequirementsWithStatusOver(float status) {
     return projectList.getAllRequirementsWithStatusOver(status);
   }
 
-  public ArrayList<Requirement> getRequirementsBeforeDeadline(MyDate deadline)
-  {
+  public ArrayList<Requirement> getRequirementsBeforeDeadline(MyDate deadline) {
     return projectList.getAllRequirementsBeforeDeadline(deadline);
   }
 
-  public Requirement getRequirementById(String Id)
-  {
-    return projectList.getRequirementById(Id);
+  public Requirement getRequirementById(String id) {
+    return projectList.getRequirementById(id);
   }
 
-  public ArrayList<Task> getAllTasks()
-  {
+  // ------------------------------ Getters for Tasks ------------------------------
+
+  public ArrayList<Task> getAllTasks() {
     return projectList.getAllTasks();
   }
 
-  public ArrayList<Task> getTasksLinkedTo(Project project)
-  {
+  public ArrayList<Task> getTasksLinkedTo(Project project) {
     return project.getAllTasks();
   }
 
-  public ArrayList<Task> getTasksLinkedTo(Requirement requirement)
-  {
+  public ArrayList<Task> getTasksLinkedTo(Requirement requirement) {
     return requirement.getAllAssignedTasks();
   }
 
-  public ArrayList<Task> getTasksLinkedTo(Member member)
-  {
+  public ArrayList<Task> getTasksLinkedTo(Member member) {
     return member.getAllAssignedTasks();
   }
 
-  public ArrayList<Task> getTasksByStatus(String status)
-  {
+  public ArrayList<Task> getTasksByStatus(String status) {
     return projectList.getAllTasksWithStatus(status);
   }
 
-  public ArrayList<Task> getTasksBeforeDeadline(MyDate deadline)
-  {
+  public ArrayList<Task> getTasksBeforeDeadline(MyDate deadline) {
     return projectList.getAllTasksBeforeDeadline(deadline);
   }
 
-  public Task getTaskById(String Id)
-  {
-    return projectList.getTaskById(Id);
+  public Task getTaskById(String id) {
+    return projectList.getTaskById(id);
   }
 
-  public ArrayList<Member> getAllMembers()
-  {
+  // ------------------------------ Getters for Members ------------------------------
+
+  public ArrayList<Member> getAllMembers() {
     return memberList.getAllMembers();
   }
 
-  public ArrayList<Member> getMembersLinkedTo(Project project)
-  {
-    ArrayList<Member> members = new ArrayList<>();
-    for (Task task : project.getAllTasks()) members.addAll(task.getAllAssignedMembers());
-    if (project.getProductOwner() != null)
-    {
-      members.add(project.getProductOwner());
-
-    }
-    if(project.getProjectCreator() != null)
-    {
-      members.add(project.getProjectCreator());
-
-    }
-    if(project.getScrumMaster() != null)
-    {
-      members.add(project.getScrumMaster());
-
-    }
-    return members;
+  public ArrayList<Member> getMembersLinkedTo(Project project) {
+    ArrayList<Member> membersToReturn = new ArrayList<>();
+    for (Task task : project.getAllTasks()) membersToReturn.addAll(task.getAllAssignedMembers());
+    if (project.getProductOwner() != null) membersToReturn.add(project.getProductOwner());
+    if(project.getProjectCreator() != null) membersToReturn.add(project.getProjectCreator());
+    if(project.getScrumMaster() != null) membersToReturn.add(project.getScrumMaster());
+    return membersToReturn;
   }
 
-  public ArrayList<Member> getMembersLinkedTo(Requirement requirement)
-  {
+  public ArrayList<Member> getMembersLinkedTo(Requirement requirement) {
     return requirement.getAllMembersWorkingOnRequirement();
   }
 
-  public ArrayList<Member> getMembersLinkedTo(Task task)
-  {
+  public ArrayList<Member> getMembersLinkedTo(Task task) {
     return task.getAllAssignedMembers();
   }
 
-  public Member getMemberByFullName(String firstName, String lastName)
-  {
+  public Member getMemberByFullName(String firstName, String lastName) {
     return memberList.getMemberByFullName(firstName,lastName);
   }
 
-  public Member getMemberByEmail(String email)
-  {
+  public Member getMemberByEmail(String email) {
     return memberList.getMemberByEmail(email);
   }
 
-  public void createProject(String title, String description, int day, int month, int year)
-  {
-    projectList.addProject(title, description, day, month, year);
+  // ------------------------------ Other Methods ------------------------------
+
+  public Project addProject(String title, String description, int day, int month, int year) {
+    return projectList.addProject(title, description, day, month, year);
   }
-  public void createProject(String title, int day, int month, int year)
-  {
-    projectList.addProject(title, day, month, year);
+
+  public Project addProject(String title, int day, int month, int year) {
+    return projectList.addProject(title, day, month, year);
   }
-  public void removeProject(Project project)
-  {
+
+  public void removeProject(Project project) {
     projectList.removeProject(project);
   }
-  public void removeProject(String Id)
-  {
+
+  public void removeProject(String Id) {
     projectList.removeProject(Id);
   }
-  public void addMember(String firstName, String lastName, MyDate birthday, String email)
-  {
+
+  public Member addMember(String firstName, String lastName, MyDate birthday, String email) {
     memberList.hireMember(firstName, lastName, birthday, email);
   }
-  public void addMember(String firstName, String lastName, MyDate birthday,String email, long telephoneNumber)
-  {
+
+  public Member addMember(String firstName, String lastName, MyDate birthday,String email, long telephoneNumber) {
     memberList.hireMember(firstName, lastName, birthday, email, telephoneNumber);
   }
-  public void removeMember(String firstName, String lastName)
-  {
+
+  public void removeMember(String firstName, String lastName) {
     memberList.fireMember(firstName, lastName);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
